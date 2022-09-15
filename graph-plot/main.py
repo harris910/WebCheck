a7e0b9ad1f9a55253fa6477263104a6066d93d34
hadiamjad <hadiyoville25@gmail.com>
2022-09-15 12:33:49 -0400

    method level graphs

--- graph-plot/main.py
+++ graph-plot/main.py
@@ -2,10 +2,10 @@ from populateGraph import createWebGraph
 from makeFeatures import createGraphFeatures
 import networkx as nx
 
+
 def main():
     createWebGraph("bundle.com")
     # createGraphFeatures("forbes.com")
-
 
 
 if __name__ == "__main__":
