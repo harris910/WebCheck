41f7cd4fc105ff31a42f4e4315c833cba232ab1f
hadiamjad <hadiyoville25@gmail.com>
2022-09-15 12:33:10 -0400

    methods

--- sele.py
+++ sele.py
@@ -21,56 +21,56 @@ df = pd.DataFrame([["bundle.com"]], columns=["website"])
 count = 0
 
 for i in df.index:
-    # try:
-    # if i < 273:
-    #     pass
-    # else:
-    dic = {}
-    # extension filepath
-    ext_file = "C:/Users/Hadiy/OneDrive/Desktop/webpage-crawler-extension/extension"
+    try:
+        # if i < 273:
+        #     pass
+        # else:
+        dic = {}
+        # extension filepath
+        ext_file = "C:/Users/Hadiy/OneDrive/Desktop/webpage-crawler-extension/extension"
 
-    opt = webdriver.ChromeOptions()
-    # devtools necessary for complete network stack capture
-    opt.add_argument("--auto-open-devtools-for-tabs")
-    # loads extension
-    opt.add_argument("load-extension=" + ext_file)
-    # important for linux
-    opt.add_argument("--no-sandbox")
+        opt = webdriver.ChromeOptions()
+        # devtools necessary for complete network stack capture
+        opt.add_argument("--auto-open-devtools-for-tabs")
+        # loads extension
+        opt.add_argument("load-extension=" + ext_file)
+        # important for linux
+        opt.add_argument("--no-sandbox")
 
-    dc = DesiredCapabilities.CHROME
-    dc["goog:loggingPrefs"] = {"browser": "ALL"}
+        dc = DesiredCapabilities.CHROME
+        dc["goog:loggingPrefs"] = {"browser": "ALL"}
 
-    os.mkdir("server/output/" + df["website"][i])
-    driver = webdriver.Chrome(
-        ChromeDriverManager().install(), options=opt, desired_capabilities=dc
-    )
-    requests.post(
-        url="http://localhost:3000/complete", data={"website": df["website"][i]}
-    )
-    driver.get(r"https://" + df["website"][i])
+        os.mkdir("server/output/" + df["website"][i])
+        driver = webdriver.Chrome(
+            ChromeDriverManager().install(), options=opt, desired_capabilities=dc
+        )
+        requests.post(
+            url="http://localhost:3000/complete", data={"website": df["website"][i]}
+        )
+        driver.get(r"https://" + df["website"][i])
 
-    # sleep
-    time.sleep(20)
+        # sleep
+        time.sleep(20)
 
-    # dictionary collecting logs
-    # 1: Logs 2: PageSource
-    dic[df["website"][i]] = []
-    # saving logs in dictionary
-    dic[df["website"][i]].append(driver.get_log("browser"))
-    dic[df["website"][i]].append(driver.page_source)
-    # saving it in csv
-    pd.DataFrame(dic).to_csv("server/output/" + df["website"][i] + "/logs.csv")
-    # driver.quit
-    driver.quit()
+        # dictionary collecting logs
+        # 1: Logs 2: PageSource
+        dic[df["website"][i]] = []
+        # saving logs in dictionary
+        dic[df["website"][i]].append(driver.get_log("browser"))
+        dic[df["website"][i]].append(driver.page_source)
+        # saving it in csv
+        pd.DataFrame(dic).to_csv("server/output/" + df["website"][i] + "/logs.csv")
+        # driver.quit
+        driver.quit()
 
-    count += 1
-    with open("logs.txt", "w") as log:
-        log.write(str(count))
-        log.close()
-    print(r"Completed: " + str(i) + " website: " + df["website"][i])
-# except:
-#     try:
-#         driver.quit()
-#     except:
-#         pass
-#     print(r"Crashed: " + str(i) + " website: " + df["website"][i])
+        count += 1
+        with open("logs.txt", "w") as log:
+            log.write(str(count))
+            log.close()
+        print(r"Completed: " + str(i) + " website: " + df["website"][i])
+    except:
+        try:
+            driver.quit()
+        except:
+            pass
+        print(r"Crashed: " + str(i) + " website: " + df["website"][i])
